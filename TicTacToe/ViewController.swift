9a6414f171ec166106977eda6f0240415fc9c36f
atalaycavusoglu <atalaycavusoglu1@gmail.com>
2023-01-04 14:59:57 +0300

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Atalay Çavuşoğlu on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
